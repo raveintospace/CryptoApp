7d24b58bfb7c4afb9faa2b9b6f11c930dbda9fbc
Uri <uriaeri@gmail.com>
2023-11-09 12:59:03 +0100

    Lesson 1 - Initial setup & ColorTheme

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//  https://youtu.be/KJoYaVnAfF8?si=y3r2PgDD05Xp9Vgr
+//  Created by Uri on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
