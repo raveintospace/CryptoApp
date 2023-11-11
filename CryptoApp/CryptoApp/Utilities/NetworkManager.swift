64d494faf41413828e39468f81bcf11110b5520c
Uri <uriaeri@gmail.com>
2023-11-11 17:15:39 +0100

    Lesson 7, pt 2 - NetworkManager finished with enum for errors

--- CryptoApp/CryptoApp/Utilities/NetworkManager.swift
+++ CryptoApp/CryptoApp/Utilities/NetworkManager.swift
@@ -1,7 +1,7 @@
 //
 //  NetworkManager.swift
 //  CryptoApp
-//
+//  Static = We don't need to initialize class NetworkManager to use its methods
 //  Created by Uri on 11/11/23.
 //
 
@@ -10,19 +10,30 @@ import Combine
 
 class NetworkManager {
     
-    // static so we don't need to initialize class NetworkManager
+    enum NetworkError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    
     static func fetch(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ try handleURLResponse(output: $0) })
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
         guard let response = output.response as? HTTPURLResponse,
               response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkError.badURLResponse(url: url)
         }
         return output.data
     }
