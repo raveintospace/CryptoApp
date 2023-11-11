f7730904cd5efccf6e0941ee2b32815b7b5d8770
Uri <uriaeri@gmail.com>
2023-11-11 17:05:58 +0100

    Lesson 7, pt 1 - NetworkManager to have reusable api call

--- CryptoApp/CryptoApp/Services/CoinDataService.swift
+++ CryptoApp/CryptoApp/Services/CoinDataService.swift
@@ -23,29 +23,13 @@ class CoinDataService {
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkManager.fetch(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    debugPrint(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: {
+                [weak self] (returnedCoins) in
                 guard let self = self else { return }
                 self.allCoins = returnedCoins
                 self.coinSubscription?.cancel() // -> cancel because it won't call api continously
-            }
+            })
     }
 }
---
+++ CryptoApp/CryptoApp/Utilities/NetworkManager.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkManager.swift
+//  CryptoApp
+//
+//  Created by Uri on 11/11/23.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager {
+    
+    // static so we don't need to initialize class NetworkManager
+    static func fetch(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            debugPrint(error.localizedDescription)
+        }
+    }
+}
