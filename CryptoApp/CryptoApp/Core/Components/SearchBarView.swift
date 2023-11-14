c1265316e6fa2fd47a31cf077f1479b64bdc9b4c
Uri <uriaeri@gmail.com>
2023-11-14 18:50:12 +0100

    Lesson 10, pt 1 - Reusable SearchBarView

---
+++ CryptoApp/CryptoApp/Core/Components/SearchBarView.swift
@@ -0,0 +1,55 @@
+//
+//  SearchBarView.swift
+//  CryptoApp
+//
+//  Created by Uri on 14/11/23.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    // Binding instead of State so searchText can bind to any string in our app
+    // Where we put a SearchBarView, we will bind a string to it
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .autocorrectionDisabled()
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)  // easier for users to tap
+                        .foregroundColor(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    
+                    ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.25),
+                        radius: 10, x: 0, y:0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(searchText: .constant(""))
+    }
+}
