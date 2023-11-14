3f38e94037f2a909dc315be57f5cd9b90e20920f
Uri <uriaeri@gmail.com>
2023-11-14 18:25:05 +0100

    Lesson 9, pt 3 - Local FileManager set in CoinImageService, to get & save images

--- CryptoApp/CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/CryptoApp/Services/CoinImageService.swift
@@ -15,14 +15,27 @@ class CoinImageService {
     
     // exclusive cancellable for getCoinImage
     private var imageSubscription: AnyCancellable?
+    
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"  // -> custom name for the folder where we store coin images
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkManager.fetch(url: url)
@@ -31,9 +44,10 @@ class CoinImageService {
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: {
                 [weak self] (returnedImage) in
-                guard let self = self else { return }
-                self.image = returnedImage
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
                 self.imageSubscription?.cancel() // -> cancel because it won't call api continously
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
         
     }
