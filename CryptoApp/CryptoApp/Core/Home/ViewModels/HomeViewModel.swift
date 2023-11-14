e8dd9cc989330b19d7d8d08009c817ab9086e1d2
Uri <uriaeri@gmail.com>
2023-11-14 19:11:28 +0100

    Lesson 11, pt 1 - Filter with Combine using text from SearchBarView & refactor the previous subscription method

--- CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -24,13 +24,36 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        
-        // subscription to allCoins array of CoinDataService
-        dataService.$allCoins
+                
+        // subscription to searchText & dataService, when any of them updates, sinks the updates
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            }
             .sink { [weak self] (returnedCoins) in
                 guard let self = self else { return }
                 self.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        // subscription to allCoins array of CoinDataService - not necessary when using our searchBar
+//        dataService.$allCoins
+//            .sink { [weak self] (returnedCoins) in
+//                guard let self = self else { return }
+//                self.allCoins = returnedCoins
+//            }
+//            .store(in: &cancellables)
     }
 }
