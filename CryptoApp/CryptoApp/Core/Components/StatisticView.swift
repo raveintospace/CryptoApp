76dc4fe346e1843cb4fcacc9ab629b2a1a939d48
Uri <uriaeri@gmail.com>
2023-11-17 18:27:58 +0100

    Lesson 12, pt 2 - StatisticView finished, hides with opacity // HomeStatsView that integrates StatisticViews, animated using its alignment

--- CryptoApp/CryptoApp/Core/Components/StatisticView.swift
+++ CryptoApp/CryptoApp/Core/Components/StatisticView.swift
@@ -19,7 +19,8 @@ struct StatisticView: View {
             Text(stat.value)
                 .font(.headline)
                 .foregroundColor(Color.theme.accent)
-            HStack {
+            
+            HStack(spacing: 4) {
                 Image(systemName: "triangle.fill")
                     .font(.caption2)
                     .rotationEffect(
@@ -30,12 +31,13 @@ struct StatisticView: View {
                     .bold()
             }
             .foregroundColor((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
         }
     }
 }
 
 struct StatisticView_Previews: PreviewProvider {
     static var previews: some View {
-        StatisticView(stat: dev.stat1)
+        StatisticView(stat: dev.stat3)
     }
 }
