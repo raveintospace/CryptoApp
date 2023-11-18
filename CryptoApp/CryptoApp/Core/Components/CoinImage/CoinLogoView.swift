6e6433ab7c72f3079843d58984b3a6ae0a5cf844
Uri <uriaeri@gmail.com>
2023-11-18 18:44:18 +0100

    Lesson 14, pt 2 - CoinLogoView for PortfolioView

---
+++ CryptoApp/CryptoApp/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,37 @@
+//
+//  CoinLogoView.swift
+//  CryptoApp
+//
+//  Created by Uri on 18/11/23.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    
+    let coin: CoinModel
+    
+    var body: some View {
+        VStack {
+            CoinImageView(coin: coin)
+                .frame(width: 50, height: 50)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.accent)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)    // scale text to fit in 1 line
+            Text(coin.name)
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinLogoView(coin: dev.coin)
+    }
+}
