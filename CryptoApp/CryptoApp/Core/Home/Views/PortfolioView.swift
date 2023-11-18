148935f211661559bdbd9523decc18e8b57c140a
Uri <uriaeri@gmail.com>
2023-11-18 18:52:43 +0100

    Lesson 14, pt 3 - coinLogoList for PortfolioView

--- CryptoApp/CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -12,20 +12,14 @@ struct PortfolioView: View {
     @Environment(\.dismiss) var dismiss
     
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
-                    
-                    ScrollView(.horizontal, showsIndicators: true, content: {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                Text(coin.symbol.uppercased())
-                            }
-                        }
-                    })
+                    coinLogoList
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -47,5 +41,31 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        })
     }
 }
