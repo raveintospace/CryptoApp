345b04ec1947d6ec57daa0ed5703a87480b4c20a
Uri <uriaeri@gmail.com>
2023-11-20 18:52:11 +0100

    Lesson 15, pt 2 - Added methods to PortfolioDataService

--- CryptoApp/CryptoApp/Services/PortfolioDataService.swift
+++ CryptoApp/CryptoApp/Services/PortfolioDataService.swift
@@ -22,9 +22,27 @@ class PortfolioDataService {
             if let error = error {
                 debugPrint("Error loading Core Data: \(error)")
             }
+            self.getPortfolio()
         }
     }
     
+    // MARK: - Public method, called from somewhere else in the app
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        
+        // check if coin is already in portfolio
+        if let entity = savedEntities.first(where: { $0.coinID == coin.id }) {
+            
+            if amount > 0 {
+                update(entity: entity, amount: amount)
+            } else {
+                delete(entity: entity)
+            }
+        } else {    // coin is not in portfolio, it's a new one to add
+            add(coin: coin, amount: amount)
+        }
+    }
+    
+    // MARK: - Private methods, only called inside the class
     private func getPortfolio() {
         let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
         
@@ -33,5 +51,36 @@ class PortfolioDataService {
         } catch let error {
             debugPrint("Error fetching Portfolio Entities: \(error)")
         }
+    }
+    
+    private func add(coin: CoinModel, amount: Double) {
+        let entity = PortfolioEntity(context: container.viewContext)
+        entity.coinID = coin.id
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func update(entity: PortfolioEntity, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func delete(entity: PortfolioEntity) {
+        container.viewContext.delete(entity)
+        applyChanges()
+    }
+    
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            debugPrint("Error saving to Core Data: \(error)")
+        }
+    }
+    
+    // save current coins to portfolio and get the info from it after being updated
+    private func applyChanges() {
+        save()
+        getPortfolio()
     }
 }
