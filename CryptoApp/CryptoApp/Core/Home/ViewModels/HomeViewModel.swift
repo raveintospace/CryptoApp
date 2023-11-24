aa5cbe408cf4e7f56b963f2286db27c118910ec2
Uri <uriaeri@gmail.com>
2023-11-24 18:02:05 +0100

    Lesson 16, pt 1 - Logic to calculate portfolioValue

--- CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -39,15 +39,6 @@ class HomeViewModel: ObservableObject {
             }
             .store(in: &cancellables)
         
-        // subscription to marketData, updates statistics array
-        marketDataService.$marketData
-            .map(mapGlobalMarketData)
-            .sink { [weak self] (returnedStats) in
-                guard let self = self else { return }
-                self.statistics = returnedStats
-            }
-            .store(in: &cancellables)
-        
         // subscription to allCoins (already filtered) & portfolioData, updates portfolioCoins
         $allCoins
             .combineLatest(portfolioDataService.$savedEntities)
@@ -65,6 +56,16 @@ class HomeViewModel: ObservableObject {
             .sink { [weak self] (returnedCoins) in
                 guard let self = self else { return }
                 self.portfolioCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        // subscription to marketData & portfolioCoins, updates statistics array
+        marketDataService.$marketData
+            .combineLatest($portfolioCoins)
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                guard let self = self else { return }
+                self.statistics = returnedStats
             }
             .store(in: &cancellables)
     }
@@ -88,7 +89,7 @@ class HomeViewModel: ObservableObject {
         }
     }
     
-    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?, portfolioCoins: [CoinModel]) -> [StatisticModel] {
         var stats: [StatisticModel] = []
         
         guard let data = marketDataModel else {
@@ -98,7 +99,15 @@ class HomeViewModel: ObservableObject {
         let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
         let volume = StatisticModel(title: "24h Volume", value: data.volume)
         let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
-        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+        
+        let portfolioValue = portfolioCoins
+            .map({ $0.currentHoldingsValue })
+            .reduce(0, +)   // add all items from map and create a unique one [Double] -> Double
+        
+        let portfolio = StatisticModel(
+            title: "Portfolio Value",
+            value: portfolioValue.asCurrencyWith2Decimals(),
+            percentageChange: 0)
         
         stats.append(contentsOf: [
             marketCap,
