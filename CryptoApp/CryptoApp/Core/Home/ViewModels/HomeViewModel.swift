63762283a9d1ba4a7fbd04770101d1148a302d38
Uri <uriaeri@gmail.com>
2023-11-25 16:33:08 +0100

    Lesson 17, pt 1 - Logic in VM to sort coins array & subscribe to changes of sort property

--- CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -11,18 +11,21 @@ import Combine
 class HomeViewModel: ObservableObject {
     
     @Published var statistics: [StatisticModel] = []
-    
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
     @Published var searchText: String = ""
-    
     @Published var isLoading: Bool = false
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()    // we won't cancel this subscription
+    
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
     
     init() {
         addSubscribers()
@@ -30,11 +33,11 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers() {
         
-        // subscription to searchText & coinData, when either of them changes, maps & sinks the updates
+        // subscription to searchText, coinData & sortOption, when any of them changes, maps & sinks the updates
         $searchText
-            .combineLatest(coinDataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins, $sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)    // -> filter after 0.5 seconds
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink { [weak self] (returnedCoins) in
                 guard let self = self else { return }
                 self.allCoins = returnedCoins
@@ -87,6 +90,25 @@ class HomeViewModel: ObservableObject {
             return coin.name.lowercased().contains(lowercasedText) ||
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
+        }
+    }
+    
+    private func filterAndSortCoins(text: String, coins: [CoinModel], sort: SortOption) -> [CoinModel] {
+        var filteredCoins = filterCoins(text: text, coins: coins)
+        let sortedCoins = sortCoins(sort: sort, coins: filteredCoins)
+        return sortedCoins
+    }
+    
+    private func sortCoins(sort: SortOption, coins: inout [CoinModel]) {    // inout -> get and return the same array after being sorted
+        switch sort {
+        case .rank, .holdings:
+            coins.sort(by: { $0.rank < $1.rank })
+        case .rankReversed, .holdingsReversed:
+            coins.sort(by: { $0.rank > $1.rank })
+        case .price:
+            coins.sort(by: { $0.currentPrice > $1.currentPrice })
+        case .priceReversed:
+            coins.sort(by: { $0.currentPrice < $1.currentPrice })
         }
     }
     
