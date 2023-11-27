537e4d4c4294488df80735f4b968e8978fc478aa
Oriol Angelet <uriaeri@gmail.com>
2023-11-27 18:23:02 +0100

    Lesson 20, pt 1 - CoinDetailView first iteration, added a LazyVGrid

--- CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
+++ CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
@@ -23,19 +23,70 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
     
-    @StateObject var vm: DetailViewModel
+    @StateObject private var vm: DetailViewModel
+    
+    // properties for LazyVGrid
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    private let spacing: CGFloat = 30
     
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
     }
     
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("")
+                    .frame(height: 150)
+                
+                Text("Overview")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: [],
+                    content: {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "title", value: "value"))
+                        }
+                    })
+                
+                Text("Additional Details")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: [],
+                    content: {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "title", value: "value"))
+                        }
+                    })
+            }
+            .padding(.leading)
+        }
+        .navigationTitle(vm.coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
