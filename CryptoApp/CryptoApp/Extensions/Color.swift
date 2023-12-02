7fd2c652e68c4eef717aae65654809385f8dcdbc
Oriol Angelet <uriaeri@gmail.com>
2023-12-02 16:02:52 +0100

    Lesson 26, pt 2 - Added ColorTheme2 to check how to update "theme"

--- CryptoApp/CryptoApp/Extensions/Color.swift
+++ CryptoApp/CryptoApp/Extensions/Color.swift
@@ -23,6 +23,15 @@ struct ColorTheme {
     let secondaryText = Color("SecondaryTextColor")
 }
 
+struct ColorTheme2 {
+    
+    let accent = Color(#colorLiteral(red: 0.4513868093, green: 0.9930960536, blue: 1, alpha: 1))
+    let background = Color(#colorLiteral(red: 0.004859850742, green: 0.09608627111, blue: 0.5749928951, alpha: 1))
+    let green = Color(#colorLiteral(red: 0.5563425422, green: 0.9793455005, blue: 0, alpha: 1))
+    let red = Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1))
+    let secondaryText = Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
+}
+
 struct LaunchTheme {
     
     let accent = Color("LaunchAccentColor")
